29abaa2ddb4d4659f3cd1ba3b2ed0a4bc54a4ade
Phony Lou <louqilin555@gmail.com>
2020-05-24 11:03:55 +0800

    refactor extract common condition

--- src/main/java/com/qilinlou/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/qilinlou/fizzbuzz/FizzBuzz.java
@@ -2,11 +2,14 @@ package com.qilinlou.fizzbuzz;
 
 public class FizzBuzz {
     public String reporting(int input) {
-        if (input % 3 == 0 && input % 5 == 0)
+        boolean isMod5 = input % 5 == 0;
+        boolean isMod3 = input % 3 == 0;
+
+        if (isMod3 && isMod5)
             return "FizzBuzz";
         if (input % 7 == 0) return "Whizz";
-        if (input % 5 == 0) return "Buzz";
-        if (input % 3 == 0) return "Fizz";
+        if (isMod3) return "Fizz";
+        if (isMod5) return "Buzz";
         return String.valueOf(input);
     }
 }
